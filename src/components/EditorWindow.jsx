67edb659ef503462e47027407e8223a96b7677a2
rishitsrivastava <rishitsrivastava43@gmail.com>
2024-01-26 23:20:21 +0530

    handeled local storage

--- src/components/EditorWindow.jsx
+++ src/components/EditorWindow.jsx
@@ -1,17 +1,48 @@
 import React, { useState } from 'react'
-import { EditorState } from 'draft-js'
-import { Editor } from 'react-draft-wysiwyg'
+import { Editor, EditorState, RichUtils, ContentState, convertToRaw } from 'draft-js'
+// import 'draft-js/dist/Draft.css'
 
 export default function EditorWindow() {
 
-    const [EditorState, setEditorState] = useState(() => {
+    const [editorState, setEditorState] = useState(() => {
         EditorState.createEmpty();
     })
+
+    const handlePreviousInput = (input, editorState) => {
+        if(input === '#' && editorState.getSelection().getStartOffset() === 0) {
+            setEditorState(RichUtils.toggleBlockType(editorState, 'header-one'));
+            return 'handled'
+        }
+        else if (input === '*' && editorState.getSelection().getStartOffset() === 0) {
+            const contentkaState = editorState.getCurrentContent();
+            const selectionkaState = editorState.getCurrentContent();
+            const blockWithEntityStart = contentkaState.getBlockWithEntityAt(selectionkaState.getStartKey());
+            const entity = blockWithEntityStart.getEntityAt(0);
+
+            if(entity && contentkaState.getEntity(entity).getType() === 'BOLD') {
+                setEditorState(RichUtils.toggleInlineStyle(editorState, 'BOLD'))
+                return 'handled';
+            }
+
+            else {
+                const contentStateWithEntity = contentState.createEntity('BOLD', 'IMMUTABLE', {});
+                const entityKey = contentStateWithEntity.getLastCreatedEntityKey();
+                const newEditorState = EditorState.push(editorState, contentStateWithEntity, 'create-entity');
+                setEditorState(RichUtils.toggleInlineStyle(newEditorState, 'BOLD', entityKey));
+                return 'handled';
+              }
+        }
+
+        else if(input === '*' && editorState.getSelection().getStartOffset() === 1) {
+            
+        }
+    }
+    
 
   return (
     <div className='m-4 mr-4! p-2 border-2 border-blue-500 w-full h-[34rem]'>
       <div
-        EditorState={EditorState}
+        editorState={editorState}
         onEditorStateChange={setEditorState} >
         
       </div>
